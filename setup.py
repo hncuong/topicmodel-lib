5adc83c00ffe84594d60809371a3bef56b2e4a4d
TruongKhang <truongkhang95@gmail.com>
2017-05-27 16:35:15 +0700

    update code in file setup.py

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     keywords=['ordination', 'LDA', 'topic modeling'],
     url="https://github.com/hncuong/topicmodel-lib",   # project home page, if any
     classifiers=[
-        'Intended Audience ::  Data Science/Research',
+        'Intended Audience ::  Science/Research',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
     ],
